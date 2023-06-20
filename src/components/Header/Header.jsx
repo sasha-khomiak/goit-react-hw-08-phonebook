0f093bfd609bd62ed95d5e785c3e7101d17abdf6
Oleksandr Khomiak <khomiak@gmail.com>
2023-06-20 19:31:59 +0300

    edit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 import { TopMenu, StyledNavLink, ButtonContainer } from './Header.styled';
 
--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const HomeView = () => {
   return <div>ви на головній сторінці</div>;
