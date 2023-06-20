50a190f138f9caf976e234b0a31fc0849beea3b4
Oleksandr Khomiak <khomiak@gmail.com>
2023-06-20 18:09:34 +0300

    edit

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -5,7 +5,9 @@ const HomeView = () => {
   return (
     <div>
       <Link to="register">register</Link>
+      <br />
       <Link to="login">register</Link>
+      <br />
       <Link to="contacts">contacts</Link>
     </div>
   );
