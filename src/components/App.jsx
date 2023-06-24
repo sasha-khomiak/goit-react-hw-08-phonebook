f3f21899ebb09e92e77d93ed8c864bf4aa17468a
Oleksandr Khomiak <khomiak@gmail.com>
2023-06-24 18:13:01 +0300

    edit lazy and little bit private route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
-import HomeView from 'pages/HomeView';
-import RegisterView from 'pages/RegisterView';
-import LoginView from 'pages/LoginView';
-import ContactsView from 'pages/ContactsView';
-import SharedLayout from './SharedLayout/SharedLayout';
+
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+
 import authOperations from 'redux/auth/authOperations';
+
+import SharedLayout from './SharedLayout/SharedLayout';
+
+import PrivateRoute from './PrivateRoute';
+
+import { lazy } from 'react';
+
+const HomeView = lazy(() => import('../pages/HomeView'));
+const RegisterView = lazy(() => import('../pages/RegisterView'));
+const LoginView = lazy(() => import('../pages/LoginView'));
+const ContactsView = lazy(() => import('../pages/ContactsView'));
+
+// // компоненти не через лейзі
+// import HomeView from 'pages/HomeView';
+// import RegisterView from 'pages/RegisterView';
+// import LoginView from 'pages/LoginView';
+// import ContactsView from 'pages/ContactsView';
+//
 
 // функціональний компонент. головний додаток, маршрутизація
 export function App() {
@@ -24,6 +39,16 @@ export function App() {
           <Route path="register" element={<RegisterView />} />
           <Route path="login" element={<LoginView />} />
           <Route path="contacts" element={<ContactsView />} />
+
+          {/* <PrivateRoute path="contacts">
+            <ContactsView />
+          </PrivateRoute> */}
+
+          {/* <Route
+            path="contact"
+            element={<PrivateRoute el={<ContactsView />} />}
+          /> */}
+
           <Route path="*" element={<HomeView />} />
         </Route>
       </Routes>
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,11 +3,15 @@ import Header from 'components/Header/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { Suspense } from 'react';
+
 const SharedLayout = () => {
   return (
     <Container>
       <Header />
-      <Outlet />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
     </Container>
   );
 };
