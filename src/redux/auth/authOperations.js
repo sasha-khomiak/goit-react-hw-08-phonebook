97049b452158d8fe4e837ad001c33a78b60c8926
Oleksandr Khomiak <khomiak@gmail.com>
2023-06-24 20:19:21 +0300

    refactoring

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -47,15 +47,15 @@ const fetchCurrentUser = createAsyncThunk(
     const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
     if (persistedToken === null || persistedToken === '') {
-      console.log('There is no persistedToken');
+      // console.log('There is no persistedToken');
       return thunkAPI.rejectWithValue();
       // return state;
     }
     token.set(persistedToken);
     try {
-      console.log('persistedToken', persistedToken);
+      // console.log('persistedToken', persistedToken);
       const { data } = await axios.get('/users/current');
-      console.log('data', data);
+      // console.log('data', data);
       return data;
     } catch (error) {}
   }
--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -27,37 +27,77 @@ const initialState = {
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
-  extraReducers: {
-    // читання контактів
-    [fetchContacts.pending]: handlePending,
-    [fetchContacts.fulfilled](state, action) {
-      state.contacts = action.payload;
-      state.isLoading = false;
-      state.error = null;
-    },
-    [fetchContacts.rejected]: handleRejected,
+  extraReducers: builder => {
+    builder
+      // новий синтаксис через builder
+      // читання контактів
+      .addCase(fetchContacts.pending, (state, action) => handlePending(state))
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.contacts = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(fetchContacts.rejected, (state, action) =>
+        handleRejected(state, action)
+      )
 
-    // додавання контакту
-    [addContact.pending]: handlePending,
-    [addContact.fulfilled](state, action) {
-      state.contacts.push(action.payload);
-      state.isLoading = false;
-      state.error = null;
-    },
-    [addContact.rejected]: handleRejected,
-
-    // видалення контакту
-    [deleteContact.pending]: handlePending,
-    [deleteContact.fulfilled](state, action) {
-      const index = state.contacts.findIndex(
-        contact => contact.id === action.payload.id
+      // додавання контакту
+      .addCase(addContact.pending, (state, action) => handlePending(state))
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.contacts.push(action.payload);
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(addContact.rejected, (state, action) =>
+        handleRejected(state, action)
+      )
+      // видалення контакту
+      .addCase(deleteContact.pending, (state, action) => handlePending(state))
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        const index = state.contacts.findIndex(
+          contact => contact.id === action.payload.id
+        );
+        state.contacts.splice(index, 1);
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(deleteContact.rejected, (state, action) =>
+        handleRejected(state, action)
       );
-      state.contacts.splice(index, 1);
-      state.isLoading = false;
-      state.error = null;
-    },
-    [deleteContact.rejected]: handleRejected,
   },
+
+  // старий синтаксис
+  // {
+  //   // читання контактів
+  //   [fetchContacts.pending]: handlePending,
+  //   [fetchContacts.fulfilled](state, action) {
+  //     state.contacts = action.payload;
+  //     state.isLoading = false;
+  //     state.error = null;
+  //   },
+  //   [fetchContacts.rejected]: handleRejected,
+
+  //   // додавання контакту
+  //   [addContact.pending]: handlePending,
+  //   [addContact.fulfilled](state, action) {
+  //     state.contacts.push(action.payload);
+  //     state.isLoading = false;
+  //     state.error = null;
+  //   },
+  //   [addContact.rejected]: handleRejected,
+
+  //   // видалення контакту
+  //   [deleteContact.pending]: handlePending,
+  //   [deleteContact.fulfilled](state, action) {
+  //     const index = state.contacts.findIndex(
+  //       contact => contact.id === action.payload.id
+  //     );
+  //     state.contacts.splice(index, 1);
+  //     state.isLoading = false;
+  //     state.error = null;
+  //   },
+  //   [deleteContact.rejected]: handleRejected,
+  // },
 });
 
 export const contactsReducer = contactsSlice.reducer;
